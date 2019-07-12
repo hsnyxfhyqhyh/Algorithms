d586ef306f70761f5c7529eec4943ed924c376c0
hsnyxfhyqhyh <xfyang46@gmail.com>
2019-07-12 10:53:20 -0400

    commit change for selection sort.

---
+++ Topics/Sorting/Project10-Sorting/02-selectionSort/com/ying/algorithm/sorting/selection01/SelectionSortingDemoPractice.java
@@ -0,0 +1,32 @@
+package com.ying.algorithm.sorting.selection01;
+
+/*
+ * To practice the sorting logic. 
+ */
+public class SelectionSortingDemoPractice {
+
+	// Driver code to test above
+	public static void main(String args[]) {
+		int arr[] = { 64, 25, 12, 22, 11 , 78, 33};
+		
+		int n = arr.length;
+
+		//TODO: sorting logic implemented here. 
+		
+		
+		
+		System.out.println("Sorted array");
+		
+		printArray(arr);
+	}
+
+
+	// Prints the array
+	private static void printArray(int arr[]) {
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+		System.out.println();
+	}
+
+}
