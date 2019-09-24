5c42304c38784c3bd53dee6f921a73e7679f636e
hsnyxfhyqhyh <xfyang46@gmail.com>
2019-09-24 10:40:19 -0400

    modify the triggers to insert into this new MZ_D3K_UPDATE table.

--- Topics/mzp-temp-backupchange/db-scripts/ist-d3realtime-20190924/step5_03_MZ_RIDER_D2000_TAIUR.sql
+++ Topics/mzp-temp-backupchange/db-scripts/ist-d3realtime-20190924/step5_03_MZ_RIDER_D2000_TAIUR.sql
@@ -11,6 +11,11 @@ Begin
    update mz_membership
    set    adm_d2000_update_dt = sysdate
    where  membership_ky   = :new.membership_ky;
+   
+   INSERT INTO MZ_PLUS_212.MZ_D3K_UPDATE (MEMBERSHIP_KY, MEMBER_KY, MEMBERSHIP_ID, PROCESS_FL, PROCESS_DT, PROCESS_KY)
+   SELECT membership_ky,member_ky,'438212'||trim(membership_id)||associate_id||check_digit_nr,'true',sysdate,MZ_D3PROCESSUPD_KY_SEQ.NEXTVAL
+		FROM MZ_PLUS_212.MZ_MEMBER
+		WHERE membership_ky=:new.membership_ky and member_ky=:new.member_ky and rider_comp_cd = 'BS';
 End;
 
 
