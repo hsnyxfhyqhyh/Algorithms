dcb5d04929c8c8f21b64984c62d3069e3b9389d5
hsnyxfhyqhyh <xfyang46@gmail.com>
2019-09-24 09:49:40 -0400

    correct some error, use the modified sequence name in step 4 script.

--- Topics/mzp-temp-backupchange/db-scripts/ist-d3realtime-20190924/step5_01_MZ_MEMBERSHIP_D2000_TBIUR.sql
+++ Topics/mzp-temp-backupchange/db-scripts/ist-d3realtime-20190924/step5_01_MZ_MEMBERSHIP_D2000_TBIUR.sql
@@ -8,7 +8,13 @@ CREATE OR REPLACE TRIGGER "MZ_MEMBERSHIP_D2000_TBIUR"
 
 
 Begin
-   :new.adm_d2000_update_dt := sysdate;
+   	:new.adm_d2000_update_dt := sysdate;
+   	--if select count(*) from MZ_PLUS_212.MZ_D3K_UPDATE where PROCESS_FL = 'true' and 
+   	INSERT INTO MZ_PLUS_212.MZ_D3K_UPDATE (MEMBERSHIP_KY, MEMBER_KY, MEMBERSHIP_ID, PROCESS_FL, PROCESS_DT, PROCESS_KY)
+   	SELECT membership_ky,member_ky,'438212'||trim(membership_id)||associate_id||check_digit_nr,'true',sysdate,MZ_D3PROCESSUPD_KY_SEQ.NEXTVAL
+		FROM MZ_PLUS_212.MZ_MEMBER
+		WHERE membership_ky=:new.membership_ky and member_type_cd='P';
+   
 End;
 
 
